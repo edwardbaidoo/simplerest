5ab453eee3b2a1028207434333e145dcc51af03b
Edward Baidoo <edwardjacobbaidoo@gmail.com>
2021-05-16 18:31:00 +0000

    changes - complete working copy

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"simplerest/routes"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:3000", r))
+}
